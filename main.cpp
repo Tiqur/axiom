76f96541b6c4c8bbd8f4321516d16c0c9e83df8c
Tiqur <Tiqur7@gmail.com>
2022-09-25 06:57:00 -0700

    Added olcPixelGameEngine

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#define OLC_PGE_APPLICATION
 #include <iostream>
 #include "./lib/Pawn.h"
 #include "./lib/King.h"
@@ -5,30 +6,42 @@
 #include "./lib/Bishop.h"
 #include "./lib/Queen.h"
 #include "./lib/Knight.h"
+#include "./lib/olcPixelGameEngine.h"
 
-
-struct Game 
+class Axiom : public olc::PixelGameEngine
 {
-  // Entire board
-  char board[64];
+  public:
+    // Entire board
+    char board[64];
 
-  // True = white
-  bool turn = true;
+    // True = white
+    bool turn = true;
 
-  Game(char* FEN, bool turn)
-  {
-    // Set game turn
-    turn = turn;
+    Axiom(char* FEN)
+    {
+      // Set app name
+      sAppName = "Axiom";
 
-    // Set game board
-    //for (int i = 0; FEN[i] != ' '; i++)
-    //{
-    //  std::cout << FEN[i] << std::endl;
-    //}
-  }
+      // Set game turn
+      turn = turn;
 
+      // Set game board
+      //for (int i = 0; FEN[i] != ' '; i++)
+      //{
+      //  std::cout << FEN[i] << std::endl;
+      //}
+    }
 
-  
+  public:
+    bool OnUserCreate() override
+    {
+      return true;
+    }
+
+    bool OnUserUpdate(float fElapsedTime) override
+    {
+      return true;
+    }
 };
 
 
@@ -36,14 +49,16 @@ int main() {
     char startingPosition[] = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
     std::cout << startingPosition << std::endl;
 
+    Axiom game = Axiom(startingPosition);
+    if (game.Construct(512, 480, 2, 2))
+      game.Start();
 
-    Game game = Game(startingPosition, true);
     //Pawn pawn = Pawn(8, true);
     //King king = King(18);
     //Rook rook = Rook(60);
     //Bishop bishop = Bishop(36);
     //Queen queen = Queen(36);
-    Knight knight = Knight(52);
+    //Knight knight = Knight(52);
 
     return 0;
 }
